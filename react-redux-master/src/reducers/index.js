daee1f0c7063f3e3ebe000dace036fc2c361f0e5
Julia Mbamarah <104596615+JuliaMbamarah@users.noreply.github.com>
2024-07-04 22:05:58 +0100

    Update index.js

--- react-redux-master/src/reducers/index.js
+++ react-redux-master/src/reducers/index.js
@@ -1,10 +1,16 @@
 //reducers
 
-const increment = (val) => {
-    return {
-        type : 'INCREMENT',
-        inc : val
+import {combineReducers} from 'redux'
+
+const counter = (state=0,action)=>{
+    if(action.type === 'INCREMENT') {
+        //This will increase the value of counter by the value passed to the increment method
+        return state+action.inc;
     }
+    //Returns the current value of the counter
+    return state;
 }
 
-export default increment;
+const myReducers = combineReducers({counter});
+
+export default myReducers;
